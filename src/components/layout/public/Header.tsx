bfa940e8f559fc924db26907814f92143adc4aa7
ehasun15019 <ehasunulislam0202@gmail.com>
2026-09-17 22:57:27 +0600

    create the dashboard

--- src/components/layout/public/Header.tsx
+++ src/components/layout/public/Header.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/svg/Logo";
 import { Button } from "@/components/ui/button";
 import { toast } from "@/components/ui/toast";
 import { useGetMe, useLogout } from "@/hooks";
+import { UserRole } from "@/types";
 import { useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 
@@ -13,9 +14,19 @@ export default function Header() {
     { name: "About us", url: "/about-us" },
   ];
 
+
+  const dashboardRoute: Record<UserRole, string> = {
+    SUPER_ADMIN: "/admin",
+    ADMIN: "/admin",
+    DOCTOR: "/doctor",
+    PATIENT: "/patient",
+  };
+
   const { data, isLoading } = useGetMe();
   const { mutate: logout } = useLogout();
   const queryClient = useQueryClient();
+
+  const role: UserRole = !!data?.data && data?.data.role;
 
   const handleLogout = () => {
     logout(undefined, {
@@ -51,6 +62,8 @@ export default function Header() {
               {route.name}
             </Link>
           ))}
+
+          {role && <Link href={dashboardRoute[role]}>Dashboard</Link>}
         </nav>
         <div>
           {!isLoading && !data && (
--- src/validation/auth.validation.ts
+++ src/validation/auth.validation.ts
@@ -1,17 +1,22 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-  email: z.email(),
-  password: z
-    .string()
-    .min(8, "Password Must Minimum 8 Characters Long.")
-    .regex(/[a-z]/, "Password must contain at least 1 Lowercase Letter")
-    .regex(/[A-Z]/, "Password must contain at least 1 Uppercase Letter")
-    .regex(/[0-9]/, "Password must contain at least 1 Number")
-    .regex(
-      /[^A-Za-z0-9]/,
-      "Password must contain at least 1 Special Character",
-    ),
+  email: z.email({
+    message: "Please provide a valid email address",
+  }),
+
+	password: z
+		.string()
+		.min(8, { message: "Password must be at least 8 characters long" })
+		.regex(/[A-Z]/, {
+			message: "Password must contain at least one uppercase letter",
+		})
+		.regex(/[0-9]/, {
+			message: "Password must contain at least one number",
+		})
+		.regex(/[^A-Za-z0-9]/, {
+			message: "Password must contain at least one special character",
+		}),
 });
 
 export const patientRegistrationSchema = z
